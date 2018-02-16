78b04c6c623b461c7468acf1c175fe9067c20835
chromia <chromia@outlook.jp>
2018-02-16 22:38:34 +0900

    add descriptions about module

--- wandplus/__init__.py
+++ wandplus/__init__.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python
+
+""":mod:`wandplus` --- Supplemental library for Wand
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+.. Wand+ : https://github.com/chromia/wandplus
+.. Wand : http://docs.wand-py.org/en/0.4.4/#
+
+"""
