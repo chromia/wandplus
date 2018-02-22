aa2b3174d0933ceee916c93670708bd7d7485a10
chromia <chromia@outlook.jp>
2018-02-23 00:41:20 +0900

    refactor a bit

--- wandplus/check.py
+++ wandplus/check.py
@@ -42,95 +42,95 @@ logo.save(filename='image/logo.png')
 text.save(filename='image/text.png')
 text_a.save(filename='image/a.png')
 
+f = adaptiveblur
 with rose.clone() as t:
-    f = adaptiveblur
     f(t, 5.0, 3.0)
     save(t, f)
 with rose.clone() as t:
     f(t, 5.0, 3.0, channel='red')
     save(t, f, True)
 
+f = adaptiveresize
 with rose.clone() as t:
-    f = adaptiveresize
     f(t, int(t.width*1.5), int(t.height*2.0))
     save(t, f)
 
+f = adaptivesharpen
 with rose.clone() as t:
-    f = adaptivesharpen
     f(t, 5, 5)
     save(t, f)
 
+f = adaptivethreshold
 with logo.clone() as t:
-    f = adaptivethreshold
     f(t, 20, 20, int(0.1*t.quantum_range))
     save(t, f)
 
+f = addnoise
 with grad.clone() as t:
-    f = addnoise
     f(t, 'gaussian')
     save(t, f)
 
+f = affinetransform
 with rose.clone() as t:
-    f = affinetransform
     with Drawing() as d:
         d.affine([2.0, 0.0, 0.0, 2.0, 0.0, 0.0])
         f(t, d)  # not work correctly
         save(t, f)
 
+f = autogamma
 with rose.clone() as t:
-    f = autogamma
     f(t)
     save(t, f)
 
+f = autolevel
 with rose.clone() as t:
-    f = autolevel
     f(t)
     save(t, f)
 
+f = blackthreshold
 with grad.clone() as t:
-    f = blackthreshold
     f(t, Color('gray(50%)'))
     save(t, f)
 
+f = blueshift
 with logo.clone() as t:
-    f = blueshift
     f(t, 0.5)
     save(t, f)
 
+f = brightnesscontrast
 with rose.clone() as t:
-    f = brightnesscontrast
     f(t, -30, 0)
     save(t, f)
 
+f = blur
 with rose.clone() as t:
-    f = blur
     f(t, 0, 3)
     save(t, f)
 
+f = charcoal
 with rose.clone() as t:
-    f = charcoal
     f(t, 5, 1)
     save(t, f)
 
+f = chop
 with grad.clone() as t:
-    f = chop
     t.gravity = 'north_west'
     f(t, 0, 00, 200, 200)
     save(t, f)
 
+f = clamp
 with rose.clone() as t:
-    f = clamp
     f(t)
     save(t, f)
 
+# f = clip
 # with rose.clone() as t:
-#     f = clip
 #     f(t)
 #     save(t, f)
 
 
+f = clut
 with grad.clone() as t:
-    f = clut
     with Image(filename='gradient:red-blue', width=1, height=100) as p:
         p.rotate(90)
         f(t, p)
@@ -150,18 +150,18 @@ xml = """
     </ColorCorrection>
 </ColorCorrectionCollection>
 """
+f = colordecisionlist
 with rose.clone() as t:
-    f = colordecisionlist
     f(t, xml)
     save(t, f)
 
+f = colorize
 with grad.clone() as t:
-    f = colorize
     f(t, Color('red'), Color('gray(25%)'))
     save(t, f)
 
+f = colormatrix
 with logo.clone() as t:
-    f = colormatrix
     kernel = [
         0.5, 0.0, 0.0, 0.0, 0.0,
         0.0, 1.5, 0.0, 0.0, 0.0,
@@ -172,34 +172,34 @@ with logo.clone() as t:
     f(t, 5, 5, kernel)
     save(t, f)
 
+f = comment
 with grad.clone() as t:
-    f = comment
     f(t, 'hello')
     save(t, f)
 
+f = constitute
 with Image() as t:
-    f = constitute
     w = 2
     h = 2
     b = b'\x00\x00\x00\xFF\xFF\xFF\xFF\x00\x00\x00\xFF\x00'
     f(t, w, h, 'RGB', 'char', b)
     save(t, f)
 
+f = contrast
 with rose.clone() as t:
-    f = contrast
     f(t, False)
     save(t, f)
 
+f = convolve
 with rose.clone() as t:
-    f = convolve
     k = [1/16, 2/16, 1/16,
          2/16, 4/16, 2/16,
          1/16, 2/16, 1/16]
     f(t, 3, k)
     save(t, f)
 
+f = cycle
 with logo.clone() as t:
-    f = cycle
     f(t, 5)
     save(t, f)
 
@@ -208,41 +208,41 @@ with rose.clone() as t:
     decipher(t, 'password')
     save(t, decipher)
 
+f = deskew
 with Image(width=80, height=40, background=Color('black')) as t:
-    f = deskew
     f(t, 0.5*t.quantum_range)  # TODO: find an skewed image as sample
     save(t, f)
 
+f = despeckle
 with rose.clone() as t:
-    f = despeckle
     # TODO: add speckle noise
     f(t)
     save(t, f)
 
+f = edge
 with logo.clone() as t:
-    f = edge
     f(t, 3)
     save(t, f)
 
+f = emboss
 with logo.clone() as t:
-    f = emboss
     f(t, 0, 3)
     save(t, f)
 
+f = enhance
 with Image(filename='plasma:', width=100, height=100) as t:
-    f = enhance
     f(t)
     save(t, f)
 
+f = extent
 with rose.clone() as t:
-    f = extent
     t.gravity = 'center'
     t.background_color = Color('blue')
     f(t, -10, -10, rose.width+20, rose.height+20)
     save(t, f)
 
+f = filterimage
 with rose.clone() as t:
-    f = filterimage
     kernel = [
         -1.0, 0.0, 1.0,
         -2.0, 0.0, 2.0,
@@ -251,39 +251,39 @@ with rose.clone() as t:
     f(t, 3, 3, kernel)
     save(t, f)
 
+f = haldclut
 with rose.clone() as t:
-    f = haldclut
     with Image(filename='hald:12') as p:
         f(t, p)
         save(t, f)
 
+f = implode
 with rose.clone() as t:
-    f = implode
     f(t, 1.0)
     save(t, f)
 
+f = label
 with rose.clone() as t:
-    f = label
     f(t, 'hello')
     save(t, f)
 
+f = localcontrast
 with logo.clone() as t:
-    f = localcontrast
     f(t, 5, 30)
     save(t, f)
 
+f = magnify
 with rose.clone() as t:
-    f = magnify
     f(t)
     save(t, f)
 
+f = minify
 with rose.clone() as t:
-    f = minify
     f(t)
     save(t, f)
 
+f = montage
 with rose.clone() as base:
-    f = montage
     with Image() as dst:
         rows = 2
         columns = 3
@@ -298,168 +298,168 @@ with rose.clone() as base:
             with f(dst, d, tile, thumb, mode, frame) as result:
                 save(result, f)
 
+f = morphology
 with logo.clone() as t:
-    f = morphology
     f(t, 'dilate', 1, 'Diamond')
     save(t, f)
 
+f = motionblur
 with logo.clone() as t:
-    f = motionblur
     f(t, 30, 10, 45)
     save(t, f)
 
+f = oilpaint
 with rose.clone() as t:
-    f = oilpaint
     f(t, 2.0)
     save(t, f)
 
+f = opaquepaint
 with logo.clone() as t:
-    f = opaquepaint
     f(t, Color('red'), Color('blue'), 1.0, False)
     save(t, f)
 
+f = orderedposterize
 with grad.clone() as t:
-    f = orderedposterize
     f(t, 'o4x4,3,3')
     save(t, f)
 
+f = polaroid
 with logo.clone() as t:
-    f = polaroid
     with Drawing() as d:
         f(t, d, 1.0)
         save(t, f)
 
+f = posterize
 with rose.clone() as t:
-    f = posterize
     f(t, 3, True)
     save(t, f)
 
+f = raiseimage
 with rose.clone() as t:
-    f = raiseimage
     f(t, 10, 10, 10, 10, True)
     save(t, f)
 
+f = randomthreshold
 with text_a.clone() as t:
-    f = randomthreshold
     rng = t.quantum_range
     f(t, int(rng * 0.05), int(rng * 0.95))
     save(t, f)
 
+f = remap
 with logo.clone() as t:
     with rose.clone() as p:
-        f = remap
         f(t, p, 'nodither')
         save(t, f)
 
+f = resample
 with rose.clone() as t:
-    f = resample
     dpi = 72 * 2
     f(t, dpi, dpi, 'lanczos', 1.0)
     save(t, f)
 
+f = roll
 with rose.clone() as t:
-    f = roll
     f(t, 10, 10)
     save(t, f)
 
+f = rotationalblur
 with rose.clone() as t:
-    f = rotationalblur
     f(t, 45)
     save(t, f)
 
+f = scale
 with rose.clone() as t:
-    f = scale
     f(t, t.width*2, t.height*2)
     save(t, f)
 
+f = segment
 with logo.clone() as t:
-    f = segment
     f(t, 'rgb', False, 5, 20)
     save(t, f)
 
+f = selectiveblur
 with logo.clone() as t:
-    f = selectiveblur
     f(t, 20, 20, 0.5*t.quantum_range)
     save(t, f)
 
+f = separate_channel
 with rose.clone() as t:
-    f = separate_channel
     f(t, 'red')
     save(t, f)
 
+f = sepiatone
 with rose.clone() as t:
-    f = sepiatone
     f(t, 0.5*t.quantum_range)
     save(t, f)
 
+f = shade
 with logo.clone() as t:
-    f = shade
     f(t, True, 45, 135)
     save(t, f)
 
+f = shadow
 with text.clone() as t:
     with text.clone() as p:
-        f = shadow
         p.negate()
         f(p, 100, 2, 10, 10)
         t.composite_channel('default_channels', p, 'overlay')
         save(t, f)
 
+f = sharpen
 with rose.clone() as t:
-    f = sharpen
     f(t, 3, 3)
     save(t, f)
 
+f = sigmoidalcontrast
 with rose.clone() as t:
-    f = sigmoidalcontrast
     f(t, True, 3, 3)
     save(t, f)
 
+f = sketch
 with logo.clone() as t:
-    f = sketch
     f(t, 10, 10, 45)
     save(t, f)
 
+f = solarize
 with rose.clone() as t:
-    f = solarize
     f(t, 0.4*t.quantum_range)
     save(t, f)
 
+f = splice
 with rose.clone() as t:
-    f = splice
     t.gravity = 'center'
     f(t, t.width//2, t.height//2, 20, 20)
     save(t, f)
 
+f = shave
 with logo.clone() as t:
-    f = shave
     f(t, 100, 100)
     save(t, f)
 
+f = shear
 with grad.clone() as t:
-    f = shear
     f(t, Color('red'), 0, 10)
     save(t, f)
 
+f = sparsecolor
 with Image(width=100, height=100, background=Color('black')) as t:
-    f = sparsecolor
     f(t, 'default_channels', 'bilinear',
       [0, 0,     1.0, 0.0, 0.0, 1.0,
        100, 100, 0.0, 1.0, 1.0, 1.0])
     save(t, f)
 
+f = spread
 with logo.clone() as t:
-    f = spread
     f(t, 'bilinear', 20)
     save(t, f)
 
+f = statistic
 with rose.clone() as t:
-    f = statistic
     f(t, 'gradient', 4, 4)
     save(t, f)
 
+f = stegano
 with rose.clone() as t:
-    f = stegano
     w = 50
     h = 40
     offset = 15
@@ -482,49 +482,49 @@ with rose.clone() as t:
         finally:
             os.remove(tmpfile)
 
+f = stereo
 with rose.clone() as t:
-    f = stereo
     with rose.clone() as p:
         p.negate()
         with f(t, p) as q:
             save(q, f)
 
+f = swirl
 with rose.clone() as t:
-    f = swirl
     f(t, 180)
     save(t, f)
 
+f = texture
 with Image(width=300, height=200) as t:
-    f = texture
     with rose.clone() as p:
         with f(t, p) as q:
             save(q, f)
 
+f = thumbnail
 with logo.clone() as t:
-    f = thumbnail
     f(t, 100, 100)
     save(t, f)
 
+f = tint
 with rose.clone() as t:
-    f = tint
     f(t, Color('rgb'), Color('gray(25%)'))
     save(t, f)
 
+f = vignette
 with logo.clone() as t:
-    f = vignette
     r = t.quantum_range
     minify(t)
     t.background_color = Color('black')
     f(t, 0, 10, 20, 20)
     save(t, f)
 
+f = wave
 with grad.clone() as t:
-    f = wave
     f(t, 40, 200)
     save(t, f)
 
+f = whitethreshold
 with grad.clone() as t:
-    f = whitethreshold
     f(t, Color('gray(50%)'))
     save(t, f)
 
